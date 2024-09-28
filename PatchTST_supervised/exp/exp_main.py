Made masks fully causal and fixed test eval

PatchTST_supervised/exp/exp_main.py
@@ -259,7 +259,7 @@ class Exp_Main(Exp_Basic):
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if 'Linear' in self.args.model or 'TST' in self.args.model:
-                            outputs = self.model(batch_x)
+                            outputs = self.model.evaluate(batch_x, self.args.pred_len)
                         else:
                             if self.args.output_attention:
                                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
@@ -267,7 +267,7 @@ class Exp_Main(Exp_Basic):
                                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 else:
                     if 'Linear' in self.args.model or 'TST' in self.args.model:
-                            outputs = self.model(batch_x)
+                            outputs = self.model.evaluate(batch_x, self.args.pred_len)
                     else:
                         if self.args.output_attention:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
@@ -326,6 +326,15 @@ class Exp_Main(Exp_Basic):
         preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
         trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
         inputx = inputx.reshape(-1, inputx.shape[-2], inputx.shape[-1])
+
+        print(preds.shape, trues.shape)
+        for i in range(10):
+            idx = i*100
+            fig, ax = plt.subplots()
+            ax.plot(trues[idx,:,0], '-k')
+            ax.plot(preds[idx,:,0], '-b')
+            #print(preds[i,:,:])
+            fig.savefig(f"test_sequence{idx}.png")
 
         # result save
         folder_path = './results/' + setting + '/'

PatchTST_supervised/layers/PatchTST_backbone.py
@@ -391,8 +391,7 @@ class _ScaledDotProductAttention(nn.Module):
         times = times.to(self.attn_decay_scale.device).to(torch.float32)
         mask = torch.zeros_like(times)
         mask[torch.abs(times)>self.attn_decay_scale] = -1*torch.inf
-        return mask
-
+        return self._enforce_causality(mask, times)
 
     def _zeta_distribution(self, r_len, c_len):
         if not self.decay_attn:
@@ -400,7 +399,8 @@ class _ScaledDotProductAttention(nn.Module):
 
         times = torch.arange(r_len).unsqueeze(1) - torch.arange(c_len).unsqueeze(0)
         times = times.to(self.attn_decay_scale.device).to(torch.float32)
-        return -1*torch.abs(times)**self.attn_decay_scale
+        mask = -1*torch.abs(times)**self.attn_decay_scale
+        return self._enforce_causality(mask, times)
 
     def _gauss_attn_decay(self, r_len, c_len):
         print("comparing adding vs multiplying attn") #DEBUG
@@ -410,11 +410,11 @@ class _ScaledDotProductAttention(nn.Module):
         #print(self.attn_decay_scale)
         times = torch.arange(r_len).unsqueeze(1) - torch.arange(c_len).unsqueeze(0)
         times = times.to(self.attn_decay_scale.device).to(torch.float32)
-        out = torch.exp(-0.5*(times/self.attn_decay_scale)**2).unsqueeze(0).unsqueeze(0)
+        mask = torch.exp(-0.5*(times/self.attn_decay_scale)**2).unsqueeze(0).unsqueeze(0)
         #print(times)
         #print(times/self.attn_decay_scale)
         #print(out)
-        return out
+        return self._enforce_causality(mask, times)
 
     def _tdist_attn_decay(self, r_len, c_len):
         print("comparing adding vs multiplying attn") #DEBUG
@@ -424,7 +424,12 @@ class _ScaledDotProductAttention(nn.Module):
         times = torch.arange(r_len).unsqueeze(1) - torch.arange(c_len).unsqueeze(0)
         times = times.to(self.attn_decay_scale.device)
         times = times.unsqueeze(0).unsqueeze(0)
-        return (1 + times**2/self.attn_decay_scale)**(-0.5*(self.attn_decay_scale + 1))
+        mask = (1 + times**2/self.attn_decay_scale)**(-0.5*(self.attn_decay_scale + 1))
+        return self._enforce_causality(mask, times)
+    
+    def _enforce_causality(self, mask, times):
+        mask[times < -1e-10] = -1*torch.inf
+        return mask
 
 
     def forward(self, q:Tensor, k:Tensor, v:Tensor, prev:Optional[Tensor]=None, key_padding_mask:Optional[Tensor]=None, attn_mask:Optional[Tensor]=None):

PatchTST_supervised/models/PatchTST.py
@@ -91,7 +91,8 @@ class Model(nn.Module):
         if self.is_sequential:
             series = x
             for t in range(target_window):
-                pred = self.forward(x[:,-self.context_window:])
+                #print(series[0,-self.context_window:,0])
+                pred = self.forward(series[:,-self.context_window:])
                 series = torch.concatenate([series, pred], dim=1)
             return series
         else:

PatchTST_supervised/run_longExp.py
@@ -113,6 +113,12 @@ if __name__ == '__main__':
         args.device_ids = [int(id_) for id_ in device_ids]
         args.gpu = args.device_ids[0]
 
+    # Sequential settings
+    if args.is_sequential:
+        input_pred_len = args.pred_len
+        args.pred_len = 1
+        args.label_len = 0
+
     print('Args in experiment:')
     print(args)
 
@@ -152,6 +158,8 @@ if __name__ == '__main__':
             exp.train(setting)
 
             print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+            if args.is_sequential:
+                exp.args.pred_len = input_pred_len
             exp.test(setting)
 
             if args.do_predict:
@@ -183,6 +191,8 @@ if __name__ == '__main__':
             setting = "Sequential_" + setting
         exp = Exp(args)  # set experiments
         print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-        exp.test(setting, test=1, save_attn=True)
+        if args.is_sequential:
+            exp.args.pred_len = input_pred_len
+        exp.test(setting, test=1, save_attn=False)
         torch.cuda.empty_cache()
         